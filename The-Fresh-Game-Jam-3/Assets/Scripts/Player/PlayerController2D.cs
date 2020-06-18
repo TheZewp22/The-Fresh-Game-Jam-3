53cd65149ccb732e13743be368d1d762f4bc3881
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-06-18 18:30:17 +1000

    Player Movement done and Platform

---
+++ The-Fresh-Game-Jam-3/Assets/Scripts/Player/PlayerController2D.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+Rigidbody2D rb2d;
+[SerializeField]
+int extraJumpsValue = 2;
+[SerializeField]
+float moveSpeed = 10f;
+[SerializeField]
+float jumpForce = 10f;
+[SerializeField]
+float checkRadius = 5f;
+[SerializeField]
+Transform groundCheckPos;
+[SerializeField]
+LayerMask whatIsGround;
+float movementX = 0f;
+int extraJumps;
+bool isGrounded = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    extraJumps = extraJumpsValue;
+    rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    isGrounded = Physics2D.OverlapCircle(groundCheckPos.position, checkRadius, whatIsGround);
+    if (isGrounded)
+    {
+    extraJumps = extraJumpsValue;
+    }
+    
+    movementX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+    if (Input.GetKeyDown("space") && extraJumps > 0)
+    {
+    Jump();
+    }    
+    }
+    void FixedUpdate()
+    {
+    rb2d.velocity = new Vector2(movementX, rb2d.velocity.y);
+    }
+    void Jump()
+    {
+    Debug.Log("Extra Jumps: " + extraJumps);
+    if (isGrounded)
+    {
+    rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+    } else {
+    rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+    extraJumps--;
+    }
+    }
+
+
+
+
+
+
+
+}
---
+++ The-Fresh-Game-Jam-3/Assets/Scripts/countdownPlatform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countdownPlatform : MonoBehaviour
+{
+[SerializeField]
+float countdownDefault = 10f;
+[SerializeField]
+Sprite spikePlatform;
+[SerializeField]
+Sprite normalPlatform;
+
+
+
+float countdown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    countdown = countdownDefault;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (countdown <= 0)
+    {
+    SwitchPlatform();
+    countdown = countdownDefault;
+    } else {
+    countdown -= Time.deltaTime;
+    }
+    void SwitchPlatform()
+    {
+    SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+    if (renderer.sprite == normalPlatform)
+    {
+    renderer.sprite = spikePlatform;
+    } else {
+    renderer.sprite = normalPlatform;
+    }
+    
+    
+    }
+
+
+
+    }
+}
